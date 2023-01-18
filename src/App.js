57d88a31016d1dc53c736709670683b3e8978804
PrangonGhose <prangonghose@gmail.com>
2023-01-19 00:59:31 +0600

    Update to use class based component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component { // eslint-disable-line
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;

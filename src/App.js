cbb930114e6f0fb7a7f4674f977d4089b8dd89b4
PrangonGhose <prangonghose@gmail.com>
2023-01-19 16:31:57 +0600

    Fix linters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 class App extends React.Component { // eslint-disable-line
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <Calculator />

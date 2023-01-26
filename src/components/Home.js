e8326322c88ad39a85371f91555fc622c3959b89
PrangonGhose <prangonghose@gmail.com>
2023-01-27 04:30:08 +0600

    Add home page

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="home-text">
+      <h1 className="heading">Welcome to our page!</h1>
+      <p className="heading">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, enim dolorum
+        architecto amet
+        sequi qui voluptas quas quam accusantium deserunt! Nemo vel ea obcaecati corporis
+        dolore quasi sunt dignissimos
+        eaque nihil assumenda voluptatem expedita dolorem, saepe reiciendis
+        consequatur soluta hic temporibus!
+        Consequatur quaerat dolore autem, non cupiditate eum nisi quod
+        illum ullam. Deleniti, ducimus ipsam.
+        Quae itaque quas voluptates ipsa?
+      </p>
+    </div>
+  );
+}

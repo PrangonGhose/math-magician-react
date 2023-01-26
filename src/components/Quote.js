8e75aa0baef826b7c7adc89c6bfc60c08f2068d2
PrangonGhose <prangonghose@gmail.com>
2023-01-27 04:30:15 +0600

    Add quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <div className="quote-text">
+      <h1 className="heading">Quote</h1>
+      <p className="heading">
+        As far as the laws of mathematics refer to reality,
+        they are not certain; and as far as they are certain,
+        they do not refer to reality.
+      </p>
+      <p className="heading author flex">- Albert Einstein</p>
+    </div>
+  );
+}
